d2f2d40e3e8ad7e35c06d80a62d07204b4de6d49
abdallahhassan169 <sovghab@gmail.com>
2023-11-17 17:39:44 +0200

    commit

--- server.js
+++ server.js
@@ -71,6 +71,27 @@ select qr_code_name from "Gym".users where id = ($1) `,
     res.status(500).send("Server error");
   }
 });
+
+app.get("/user_img", async (req, res) => {
+  try {
+    const user_id = req.query.id;
+
+    //selecting data
+
+    const { rows } = await pool.query(
+      `   
+select img from "Gym".users where id = ($1) `,
+      [user_id]
+    );
+    console.log(rows);
+    //mapping and cast the date
+
+    res.sendFile(`${rows[0]?.img}`);
+  } catch (error) {
+    console.error("Error fetching visit_visitors data:", error);
+    res.status(500).send("Server error");
+  }
+});
 app.get("/user_by_id", async (req, res) => {
   try {
     console.log("Fetching types data...");
@@ -116,31 +137,40 @@ select * from "Gym".users
       [code]
     );
 
-    const { log } = await pool.query(
-      `   
+    console.log(rows, "rows");
+    //mapping and cast the date
+    if (rows[0]?.reg_end_date > date) {
+      console.log(rows[0].reg_end_date > date);
+      console.log(rows[0].reg_end_date, date);
+      const new_classes_num = parseInt(rows[0].classes_num) - 1;
+      const { log } = await pool.query(
+        `   
           INSERT INTO "Gym".users_log(
 	 user_id, "time")
 	VALUES ((select id from "Gym".users where qr_code =($1) ) , (CURRENT_TIMESTAMP ))
  
  `,
-      [code]
-    );
-    console.log(rows, "rows");
-    //mapping and cast the date
-    if ((rows[0].classes_num > 0) & (rows[0].reg_end_date > date.getDate())) {
-      const new_classes_num = parseInt(rows[0].classes_num) - 1;
-      const { update } = await pool.query(
-        `   
+        [code]
+      );
+
+      if (rows[0].classes_num > 0) {
+        const { update } = await pool.query(
+          `   
 update   "Gym".users set classes_num =($1)
  where qr_code = ($2) returning id
  `,
-        [new_classes_num, code]
-      );
-      console.log(update);
+          [new_classes_num, code]
+        );
+      }
+
       res.send({ id: String(rows[0].id), exist: true });
     } else {
       console.log(rows);
-      res.send({ id: String(rows[0].id), exist: false });
+      res.send(
+        rows[0]?.id
+          ? { id: String(rows[0]?.id), exist: false }
+          : { exist: false }
+      );
     }
   } catch (error) {
     console.error("Error fetching visit_visitors data:", error);
@@ -218,11 +248,11 @@ app.get("/product_prices", async (req, res) => {
     //selecting data
     const { rows } = await pool.query(
       `
-      select pp.*,pr.name from "Gym"."Product_Price" pp join "Gym"."Products" pr
+      select pp.*,pr.name from "Gym"."Product_Transactions" pp join "Gym"."Products" pr
       on pr.id = pp.product_id
         where
        pr.name like concat('%',cast(($1)as text),'%')
-      
+      and type = 1
         order by pp.id desc limit ($2) offset ($3)   
   ; `,
       [query, req.query.limit, req.query.offset]
@@ -346,14 +376,18 @@ app.get("/product_reports", async (req, res) => {
     const { rows } = await pool.query(
       `
 SELECT  COALESCE(sub.date_trunc ) as date
-,coalesce(sub.sum,0) as sum 
-
+,coalesce(sub.sum,0) as profit , coalesce(cost_sum.shipment_sum,0) as cost 
+,(coalesce(sub.sum,0)  - coalesce(cost_sum.shipment_sum,0) ) as total
 FROM
   (SELECT DATE_TRUNC(($1), date) AS date_trunc, SUM(total_price) AS sum
    FROM "Gym"."Product_Transactions" where  date between
-  cast(($2) as date  ) and  cast( ($3) as date  )
-   GROUP BY DATE_TRUNC(($1), date)  ) AS sub 
-
+  cast(($2) as date  ) and  cast( ($3) as date  ) and type = 2
+   GROUP BY DATE_TRUNC(($1), date)  ) AS sub
+   left join (SELECT DATE_TRUNC(($1), date) AS date_trunc, SUM(total_price) AS shipment_sum
+   FROM "Gym"."Product_Transactions" where  date between
+  cast(($2) as date  ) and  cast( ($3) as date  ) and type = 1
+   GROUP BY DATE_TRUNC(($1), date)  ) as cost_sum
+      on cost_sum.date_trunc = sub.date_trunc 
 order by 1 desc limit ($4) offset($5) ;
   ; `,
       [type, from, to, req.query.limit, req.query.offset]
@@ -383,10 +417,9 @@ app.get("/product_transactions", async (req, res) => {
 
 SELECT t.* , p.name
    FROM "Gym"."Product_Transactions" t
-   left join "Gym"."Product_Price" pp on t.product_price_id = pp.id
-  left join "Gym"."Products" p on pp.product_id = p.id
+  left join "Gym"."Products" p on t.product_id = p.id
      where  t.date between
-  cast(($1) as date  ) and  cast( ($2) as date  )
+  cast(($1) as date  ) and  cast( ($2) as date  ) and type = 2
 order by t.id desc limit ($3) offset($4) ;
   ; `,
       [from, to, req.query.limit, req.query.offset]
@@ -401,28 +434,51 @@ order by t.id desc limit ($3) offset($4) ;
   }
 });
 
-app.post("/add_user", async (req, res) => {
+app.post("/add_user", upload.single("image"), async (req, res) => {
   try {
-    console.log(req.body);
-    const dataString = JSON.stringify(req.body);
+    console.log(req.body.data, "jsonData");
+
+    const imageFile = req?.file ?? null;
+    const jsonData = JSON.parse(req.body.data);
+    const dataString = JSON.stringify(jsonData);
+
+    const data = [
+      jsonData.name,
+      jsonData.email,
+      jsonData.phone,
+      jsonData.birth_day,
+      jsonData.id_user_type,
+      dataString,
+      `${dataString}.png`,
+      jsonData.id_sport,
+      0,
+    ];
+    if (imageFile) {
+      const extension = extname(imageFile?.originalname); // Get the file extension
+
+      // Rename the uploaded file with the extension
+      const newFileName = `${imageFile?.filename}${extension}`;
+      const newFilePath = join(
+        dirname(fileURLToPath(import.meta.url)),
+        "users",
+        newFileName
+      );
+      data.push(newFilePath);
+      fs.renameSync(imageFile?.path, newFilePath);
+    } else data.push(null);
     //selecting data
+
     const { rows } = await pool.query(
       `   
 INSERT INTO "Gym".users(
-	 name, email, phone, birth_day,  id_user_type ,qr_code , qr_code_name,id_sport, classes_num)
-	VALUES ($1, $2,  $3, $4 , $5 ,$6 ,$7,$8,$9)  returning id `,
-      [
-        req.body.name,
-        req.body.email,
-        req.body.phone,
-        req.body.birth_day,
-        req.body.id_user_type,
-        dataString,
-        `${req.body.email}.png`,
-        req.body.id_sport,
-        0,
-      ]
+	 name, email, phone, birth_day,  id_user_type ,qr_code , qr_code_name,id_sport, classes_num , img)
+	VALUES ($1, $2,  $3, $4 , $5 ,$6 ,$7,$8,$9 , $10)  returning id `,
+      data
     );
+
+    res.json("inserted succefully");
+    console.log(data);
+    //selecting data
 
     const qrCodeImage = await QRCode.toFile(
       `qrs/${req.body.email}.png`,
@@ -442,14 +498,29 @@ app.post("/add_product", async (req, res) => {
   try {
     console.log(req.body);
     //selecting data
-    const { rows } = await pool.query(
-      `   
+    if (!req.body.id) {
+      const { rows } = await pool.query(
+        `   
 INSERT INTO "Gym"."Products"(
-	 name , serial) VALUES ($1, $2)
+	 name , serial , availble , price) VALUES ($1, $2 , $3 , $4)
     returning id `,
-      [req.body.name, req.body.serial]
-    );
-
+        [req.body.name, req.body.serial, req.body.qty, req.body.price]
+      );
+    } else {
+      const { rows } = await pool.query(
+        `   
+UPDATE "Gym"."Products"
+	SET name=($1), serial=($2), availble=($3), price=($4)
+	WHERE id = ($5) `,
+        [
+          req.body.name,
+          req.body.serial,
+          req.body.qty,
+          req.body.price,
+          req.body.id,
+        ]
+      );
+    }
     res.json("add succefully");
   } catch (error) {
     console.error("Error fetching visit_visitors data:", error);
@@ -464,19 +535,17 @@ app.post("/add_product_transaction", async (req, res) => {
     const { rows } = await pool.query(
       `   
 INSERT INTO "Gym"."Product_Transactions"(
-	 qty , product_price_id , total_price , date ) VALUES ($1, $2 , $3 , CURRENT_TIMESTAMP)
+	 qty , product_id , total_price , date , type ) VALUES ($1, $2 , $3   , CURRENT_TIMESTAMP , $4)
     returning id `,
-      [
-        req.body.qty,
-        req.body.product_price_id,
-        req.body.qty * req.body.unit_price,
-      ]
+      [req.body.qty, req.body.product_id, req.body.qty * req.body.unit_price, 2]
     );
-    const { upd } = await pool.query(
-      `   
-update "Gym"."Product_Price" set qty = qty - ($1) where id = ($2) `,
-      [parseInt(req.body.qty), parseInt(req.body.product_price_id)]
-    );
+    if (rows[0].id) {
+      const { upd } = await pool.query(
+        `   
+update "Gym"."Products" set availble = availble - ($1) where id = ($2) `,
+        [parseInt(req.body.qty), parseInt(req.body.product_id)]
+      );
+    }
     res.json("add succefully");
   } catch (error) {
     console.error("Error fetching visit_visitors data:", error);
@@ -484,38 +553,59 @@ update "Gym"."Product_Price" set qty = qty - ($1) where id = ($2) `,
   }
 });
 
-app.post("/add_product_price", async (req, res) => {
+app.post("/add_shipment", async (req, res) => {
   try {
     console.log(req.body);
     const date = new Date();
-    if (!req.body.id) {
-      const { rows } = await pool.query(
-        `   
-INSERT INTO "Gym"."Product_Price"(
-	 qty,product_id , unit_price , total_price , date , total_qty ) VALUES ($1, $2,$3,$4,$5,$6)
-    returning id `,
-        [
-          req.body.qty,
-          req.body.product_id,
-          req.body.unit_price,
-          req.body.total_price,
-          date.toISOString().slice(0, 18),
-          req.body.qty,
-        ]
-      );
 
-      res.json("add succefully");
-    } else {
-      const { rows } = await pool.query(
+    const { rows } = await pool.query(
+      `   
+INSERT INTO "Gym"."Product_Transactions"(
+	 qty, product_id, total_price, date, type)
+	VALUES ( $1, $2, $3, $4, $5)
+    returning id; `,
+      [
+        req.body.qty,
+        req.body.product_id,
+        req.body.total_price,
+        date.toISOString().slice(0, 18),
+        1,
+      ]
+    );
+    console.log(rows);
+    if (rows[0].id) {
+      const { update_qty } = await pool.query(
         `   
-update "Gym"."Product_Price" 
-	 set qty = ($1)  , unit_price  = ($2) , total_price =($3) where id = ($4)  
-    returning id `,
-        [req.body.qty, req.body.unit_price, req.body.total_price, req.body.id]
+update "Gym"."Products" set availble = availble + ($1) where id = ($2) returning availble `,
+        [req.body.qty, req.body.product_id]
       );
-
-      res.json("add succefully");
     }
+    res.json("add succefully");
+  } catch (error) {
+    console.error("Error fetching visit_visitors data:", error);
+    res.status(500).send("Server error");
+  }
+});
+
+app.post("/add_product_transaction", async (req, res) => {
+  try {
+    console.log(req.body);
+    //selecting data
+    const { rows } = await pool.query(
+      `   
+INSERT INTO "Gym"."Product_Transactions"(
+	 qty , product_id , total_price , date , type ) VALUES ($1, $2 , $3   , CURRENT_TIMESTAMP , $4)
+    returning id `,
+      [req.body.qty, req.body.product_id, req.body.qty * req.body.unit_price, 2]
+    );
+    if (rows[0].id) {
+      const { upd } = await pool.query(
+        `   
+update "Gym"."Products" set availble = availble - ($1) where id = ($2) `,
+        [parseInt(req.body.qty), parseInt(req.body.product_id)]
+      );
+    }
+    res.json("add succefully");
   } catch (error) {
     console.error("Error fetching visit_visitors data:", error);
     res.status(500).send("Server error");
